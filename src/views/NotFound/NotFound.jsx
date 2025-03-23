99d215bb5e8e2fb6672378ff3f9e24d73f97f5d6
rushLovenshier <ruwandi.2001.18@gmail.com>
2025-03-23 20:34:04 +0530

    Not_found UI Development

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>404</h1>
-      <p>Page Not Found</p>
+    <div className="not-found-container">
+      <div className="not-found-content">
+        <h1 className="not-found-header">404 <br></br>Page Not Found</h1>
+        <img src="/src/assets/not_found_page.gif" alt="Not Found" className="not-found-image" />
+        <p className="not-found-description">
+          The page you're looking for doesn't exist or has been moved.
+        </p>
+        <p className="not-found-suggestion">
+          You can go back to the <a href="/">Home Page</a>.
+        </p>
+      </div>
     </div>
   );
 };
