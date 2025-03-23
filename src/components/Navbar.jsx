0207d506cfcdabeba3f6ff77a528c310bc3399c8
rushLovenshier <ruwandi.2001.18@gmail.com>
2025-03-23 20:34:39 +0530

    changes to Nav Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,29 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import logo from "../assets/logo.png"; 
 
 export default function Navbar() {
   return (
     <nav className="nav">
       <Link to="/" className="site-title">
+        <img src={logo} alt="VisionAbixo Logo" className="logo" />
         VisionAbixo
       </Link>
       <ul>
         <CustomLink to="/Service">Service</CustomLink>
         <CustomLink to="/About">About</CustomLink>
         <CustomLink to="/Help">Help</CustomLink>
-
       </ul>
     </nav>
-  )
+  );
 }
 
-function CustomLink({ to, children, ...props }) {
-  const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+function CustomLink({ to, children }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
   return (
     <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
-        {children}
-      </Link>
+      <Link to={to}>{children}</Link>
     </li>
-  )
+  );
 }
